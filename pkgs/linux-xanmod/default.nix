3fd35350f87dcc1ebc66111a340dc8fec749a0b4
Volodia P.-G <volodia.parol-guarino@proton.me>
2022-11-13 23:40:25 +0100

    Update kernel and bore

--- pkgs/linux-xanmod/default.nix
+++ pkgs/linux-xanmod/default.nix
@@ -11,18 +11,20 @@
 let
   _major = "6";
   _minor = "0";
-  _rc = "7";
+  _rc = "8";
 
   major = "${_major}.${_minor}";
   minor = _rc;
   version = "${major}.${minor}";
   release = "1";
+  
+  bore_version = "1.6.34.0";
 
   patches-src = fetchFromGitHub {
     owner = "firelzrd";
     repo = "bore-scheduler";
-    rev = "18146e79ec617d48c5ebccf9da6976758c5cf26a";
-    sha256 = "sha256-cMDZjA64QofcNRdxh2FNcF8yeNPiYiWZYDB5xgz2f9c=";
+    rev = "e097de64f6cece82a9b80edf6e8dc2803383807a";
+    sha256 = "sha256-NBe87JpPZZmqtKgGsCRTZrQiwvr3wJXGky+fHnYeTYo=";
   };
 
   # https://github.com/NixOS/nixpkgs/pull/129806
@@ -49,7 +51,7 @@ buildLinux {
     owner = "xanmod";
     repo = "linux";
     rev = "${version}-xanmod${release}";
-    sha256 = "sha256-qeM2oswuop42rvyBGlrH6VvODScLCpAOjTc4KR5a2Ec=";
+    sha256 = "sha256-1ZHJ6Qu/MDcSwQjfQws3v/ZFECkUOGhIN8Z8J+nCIYc=";
   };
   modDirVersion = "${version}-volodiapg-bore-xanmod${release}";
 
@@ -71,23 +73,22 @@ buildLinux {
       inherit name;
       patch = name;
     })
-    (lib.filesystem.listFilesRecursive "${patches-src}/bore"));
-    # [
-    #   # Block patches. Set BFQ as default
-    #   # "${patches-src}/${major}/block/0001-block-Kconfig.iosched-set-default-value-of-IOSCHED_B.patch"
-    #   # "${patches-src}/${major}/block/0002-block-Fix-depends-for-BLK_DEV_ZONED.patch"
-    #   # "${patches-src}/${major}/block/0002-LL-elevator-set-default-scheduler-to-bfq-for-blk-mq.patch"
-    #   # "${patches-src}/${major}/block/0003-LL-elevator-always-use-bfq-unless-overridden-by-flag.patch"
+    # (lib.filesystem.listFilesRecursive "${patches-src}/bore"));
+    [
+      # Block patches. Set BFQ as default
+      # "${patches-src}/${major}/block/0001-block-Kconfig.iosched-set-default-value-of-IOSCHED_B.patch"
+      # "${patches-src}/${major}/block/0002-block-Fix-depends-for-BLK_DEV_ZONED.patch"
+      # "${patches-src}/${major}/block/0002-LL-elevator-set-default-scheduler-to-bfq-for-blk-mq.patch"
+      # "${patches-src}/${major}/block/0003-LL-elevator-always-use-bfq-unless-overridden-by-flag.patch"
 
-    #   # "${patches-src}/${major}/intel/xanmod/0001-intel_rapl-Silence-rapl-trace-debug.patch"
-    #   # "${patches-src}/${major}/intel/xanmod/0002-firmware-Enable-stateless-firmware-loading.patch "
-    #   # "${patches-src}/${major}/intel/xanmod/0003-locking-rwsem-spin-faster.patch"
-    #   # "${patches-src}/${major}/intel/xanmod/0004-drivers-initialize-ata-before-graphics.patch"
-    #   # "${patches-src}/${major}/intel/xanmod/0005-init-wait-for-partition-and-retry-scan.patch"
+      # "${patches-src}/${major}/intel/xanmod/0001-intel_rapl-Silence-rapl-trace-debug.patch"
+      # "${patches-src}/${major}/intel/xanmod/0002-firmware-Enable-stateless-firmware-loading.patch "
+      # "${patches-src}/${major}/intel/xanmod/0003-locking-rwsem-spin-faster.patch"
+      # "${patches-src}/${major}/intel/xanmod/0004-drivers-initialize-ata-before-graphics.patch"
+      # "${patches-src}/${major}/intel/xanmod/0005-init-wait-for-partition-and-retry-scan.patch"
 
-    #   "${patches-src}/${major}/misc/0001-Add-latency-priority-for-CFS-class.patch"
-    #   "${patches-src}/${major}/sched/0001-bore-cachy.patch"
-    # ]);
+      "${patches-src}/bore/0001-linux6.0.y-bore${bore_version}.patch"
+    ]);
 
 
 
